d5706e9a2f2fa71aa70f0cc019bfdf9e48507a61
Alamelusharmada <30341306+Alamelusharmada@users.noreply.github.com>
2017-07-21 16:09:43 +0530

    Create leap.c

---
+++ leap.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    scanf("%d",&i);
+    if(i%2==0)
+    {
+        printf("the no is even");
+    }
+    else
+    {
+        printf("the no is odd");
+    }
+    return 0;
+    
+}
