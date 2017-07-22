0e6bef2eca98d244dead50c102e63de3e0bab86e
Alamelusharmada <30341306+Alamelusharmada@users.noreply.github.com>
2017-07-22 15:13:53 +0530

    Rename leap.c to leapyear.c

---
+++ leapyear.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%4==0)
+    {
+        if(a%100==0)
+        {
+            if(a%400==0)
+            printf("it is a leap year");
+            else
+            printf("it is not a leap year");
+        }
+            else
+            printf("it is  a leap year");
+    }
+        else
+        printf("it is not a leap year");
+    
+    return 0;
+}
