17a9950d7b7ce484f5dc4295ca2d78264dcedeb5
Alamelusharmada <30341306+Alamelusharmada@users.noreply.github.com>
2017-07-23 09:40:43 +0530

    Create largest.c

---
+++ largest.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int a,b,c;
+    scanf("%d %d %d",&a,&b,&c);
+    if(a>=b&&a>=c)
+    printf("a is big");
+    if(b>=a&&b>=c)
+    printf("b is big");
+    if(c>=a&&c>=b)
+    printf("c is big");
+    return 0;
+}
